18541b04cd35ccacfecaf341ff9b066dba860d01
Cristian Mardare <cristian.mardare.dev@gmail.com>
2020-03-07 19:46:24 +0200

    test: use 'sharp' to validate response image metadata

--- tests/integration/image.routes.test.ts
+++ tests/integration/image.routes.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import path from 'path'
 import sharp from 'sharp'
 import fs from 'fs'
+import { promisify } from 'util'
 
 import { server } from '../../src/api/server'
 
@@ -17,6 +18,8 @@ const binaryParser = function (res, cb) {
 	  cb(null, new Buffer(res.data, "binary"));
 	});
       };
+
+const unlink = promisify(fs.unlink)
 
 describe('GET /api/image', () => {
 	const ROUTE = '/api/image'
@@ -46,7 +49,12 @@ describe('GET /api/image', () => {
 
 	describe('with file name pointing to existing file', () => {
 		var FILE_PATH = 'N/A'
-		var FILE_NAME = 'test_file.png'
+		var FILE_NAME_NO_EXT = 'test_file'
+		var EXTENSIONS = {
+			PNG: 'png',
+			JPG: 'jpg'
+		}
+		var FILE_NAME = `${FILE_NAME_NO_EXT}.${EXTENSIONS.PNG}`
 
 		const DEFAULT_WIDTH = 110
 		const DEFAULT_HEIGHT = 100
@@ -56,8 +64,8 @@ describe('GET /api/image', () => {
 			FILE_PATH = path.join(global.__basedir, 'repository', FILE_NAME)
 		})
 
-		beforeEach((done) => {
-			sharp(FILE_NAME, {
+		beforeEach(() => {
+			return sharp(FILE_NAME, {
 				create: {
 				  width: DEFAULT_WIDTH,
 				  height: DEFAULT_HEIGHT,
@@ -65,9 +73,7 @@ describe('GET /api/image', () => {
 				  background: { r: 255, g: 0, b: 0, alpha: 0.5 }
 				}
 			      })
-			      .pipe(fs.createWriteStream(FILE_PATH))
-			      .on('finish', done)
-			      .on('error', done)
+			      .toFile(FILE_PATH)
 		})
 
 		test('should successfully return original file contents (file name as part of path)', async () => {
@@ -78,7 +84,11 @@ describe('GET /api/image', () => {
 						.parse(binaryParser)
 
 			expect(resp.status).toEqual(200)
-			expect(resp.body.length).toEqual(DEFAULT_WIDTH*DEFAULT_HEIGHT*DEFAULT_CHANNELS)
+
+			let meta = await sharp(resp.body).metadata()
+			expect(meta.width).toEqual(DEFAULT_WIDTH)
+			expect(meta.height).toEqual(DEFAULT_HEIGHT)
+			expect(meta.format).toEqual(EXTENSIONS.PNG)
 		})
 
 		test('should successfully return original file contents (file name as query string parameter)', async () => {
@@ -89,12 +99,52 @@ describe('GET /api/image', () => {
 						.parse(binaryParser)
 
 			expect(resp.status).toEqual(200)
-			expect(resp.body.length).toEqual(DEFAULT_WIDTH*DEFAULT_HEIGHT*DEFAULT_CHANNELS)
+			
+			let meta = await sharp(resp.body).metadata()
+			expect(meta.width).toEqual(DEFAULT_WIDTH)
+			expect(meta.height).toEqual(DEFAULT_HEIGHT)
+			expect(meta.format).toEqual(EXTENSIONS.PNG)
 		})
 
-		afterEach((done) => {
-			console.log('unlink')
-			fs.unlink(FILE_PATH, done)
+		describe('given new valid size', () => {
+			const resizeBy = 0.5
+			const NEW_WIDTH = resizeBy * DEFAULT_WIDTH
+			const NEW_HEIGHT = resizeBy * DEFAULT_HEIGHT
+
+			test('should successfully return resized file (file name as query string parameter)', async () => {
+				let path = `${ROUTE}?file=${FILE_NAME}&size=${NEW_WIDTH}x${NEW_HEIGHT}`
+
+				const resp = await request(server)
+						.get(path)
+						.buffer()
+						.parse(binaryParser)
+
+				expect(resp.status).toEqual(200)
+
+				let meta = await sharp(resp.body).metadata()
+				expect(meta.width).toEqual(NEW_WIDTH)
+				expect(meta.height).toEqual(NEW_HEIGHT)
+				expect(meta.format).toEqual(EXTENSIONS.PNG)
+			})
+
+			afterEach(async () => {
+				const resizedFilePath = path.join(global.__basedir, 'repository', `${FILE_NAME_NO_EXT}_${NEW_WIDTH}_${NEW_HEIGHT}.${EXTENSIONS.PNG}`)
+				try{
+					await unlink(resizedFilePath)
+				}
+				catch(err){
+					// ignore error	
+				}
+			}) 
+		})
+
+		afterEach(async () => {
+			try{
+				await unlink(FILE_PATH)
+			}
+			catch(err){
+				// ignore error	
+			}
 		})
 	})
 })
