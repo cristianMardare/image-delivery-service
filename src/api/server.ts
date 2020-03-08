941963ddfc8dd21d989ce9bdd59b4c3c3c4644e2
Cristian Mardare <cristian.mardare.dev@gmail.com>
2020-03-08 20:54:15 +0200

    docs: document api
    API is documented using OpenAPI (https://swagger.io/resources/open-api/)
    Documentation is exposed on endpoint: /api/docs

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,18 @@
-import 'module-alias/register';
+import 'module-alias/register'
+
 import express from 'express'
+import path from 'path'
+import swaggerUi from 'swagger-ui-express'
+import yamljs from 'yamljs'
 
 import { AppRouter } from './routes'
 import cacheStats from './middleware/cache-stats'
 
+const apiDocumentation = yamljs.load(path.join(__dirname, 'api-description.yaml'))
 const app = express()
 
 app.use(cacheStats)
 app.use(AppRouter)
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 
 export const server: express.Express = app
